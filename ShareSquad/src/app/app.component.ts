059ac7f710afe6f7c7fbacb4fe6276b553147c1d
SunitaKumari <172397566+sunitakumari22@users.noreply.github.com>
2025-05-24 12:11:37 +0530

    Created  home component and  required  changes related thsi

--- ShareSquad/src/app/app.component.ts
+++ ShareSquad/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/header/navbar/navbar.component";
+import { HomeComponent } from "./components/shared/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [NavbarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
